10839fbeee4450eded1676312eda8b3568b7d9ec
Harris Ingram <harrisdaniel@hotmail.com>
2016-08-08 22:52:08 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @version 0.2.1
+ * @package Tilde.TimeManagement
+ * 
+ * @copyright   Copyright (C) 2016 Team Tilde Time, All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE
+ */
+ 
+ /**
+  * Constant that is checked in all included files to prevent direct access.
+  * Emulated from that which is used by the Joomla! framework
+  */
+define('TILDE_TIME', 1);
+
+define('TILDE_PATH_BASE', __DIR__);
+
+require_once 'includes/defines.php';
+
+$application = TildeFactory::getApplication('site');
+
+$application->execute();
