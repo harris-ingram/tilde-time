add523692f949e164ac294b46930b525ad322362
Harris Ingram <harrisdaniel@hotmail.com>
2016-08-08 22:53:59 +1000

    Create defines.php

---
+++ includes/defines.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @version 0.2.1
+ * @package Tilde.TimeManagement
+ * 
+ * @copyright   Copyright (C) 2016 Team Tilde Time, All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE
+ */
+ 
+defined('TILDE_TIME') or die;
+
+$parts = explode(DIRECTORY_SEPARATOR, TILDE_PATH_BASE);
+
+define('TILDE_PATH_ROOT', implode(DIRECTORY_SEPARATOR,$parts));
+define('TILDE_PATH_SITE', TILDE_PATH_ROOT);
+define('TILDE_PATH_ADMINISTRATOR', TILDE_PATH_ROOT . DIRECTORY_SEPARATOR . 'administrator');
+define('TILDE_PATH_LIBRARIES', TILDE_PATH_ROOT . DIRECTORY_SEPARATOR . 'libraries');
+define('TILDE_PATH_PAGES', TILDE_PATH_ROOT . DIRECTORY_SEPARATOR . 'pages');
