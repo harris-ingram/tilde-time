ec04af28e6216976ae88a4ba78bfe17ea779642a
Harris Ingram <harrisdaniel@hotmail.com>
2016-08-08 22:54:53 +1000

    Create framework.php

---
+++ includes/framework.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version 0.2.1
+ * @package Tilde.TimeManagement
+ * 
+ * @copyright   Copyright (C) 2016 Team Tilde Time, All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE
+ */
+ 
+defined('TILDE_TIME') or die;
+ 
+require_once 'settings.php';
+
+require_once 'includes.php';
