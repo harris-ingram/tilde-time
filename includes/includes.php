f074c00b80b138c34e9916c2f98c5a656f9c5749
Harris Ingram <harrisdaniel@hotmail.com>
2016-08-08 22:55:26 +1000

    Create includes.php

---
+++ includes/includes.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @version 0.2.1
+ * @package Tilde.TimeManagement
+ * 
+ * @copyright   Copyright (C) 2016 Team Tilde Time, All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE
+ */
+
+defined('TILDE_TIME') or die; 
+ 
+require_once TILDE_PATH_LIBRARIES.'/application/cms.php';
+require_once TILDE_PATH_LIBRARIES.'/application/site.php';
+require_once TILDE_PATH_LIBRARIES.'/application/administrator.php';
+require_once TILDE_PATH_LIBRARIES.'/document.php';
+require_once TILDE_PATH_LIBRARIES.'/database.php';
+require_once TILDE_PATH_LIBRARIES.'/session.php';
+require_once TILDE_PATH_LIBRARIES.'/user.php';
+require_once TILDE_PATH_LIBRARIES.'/message.php';
+require_once TILDE_PATH_LIBRARIES.'/category.php';
+require_once TILDE_PATH_LIBRARIES.'/group.php';
+require_once TILDE_PATH_LIBRARIES.'/tag.php';
+require_once TILDE_PATH_LIBRARIES.'/select.php';
