c501ad93262ef28644b7c97aa5f1a137fdb5e984
Harris Ingram <harrisdaniel@hotmail.com>
2016-08-08 23:19:44 +1000

    Update includes.php

--- includes/includes.php
+++ includes/includes.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * @version 0.2.1
@@ -6,18 +7,17 @@
  * @copyright   Copyright (C) 2016 Team Tilde Time, All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE
  */
-
 defined('TILDE_TIME') or die; 
  
 require_once TILDE_PATH_LIBRARIES.'/application/cms.php';
 require_once TILDE_PATH_LIBRARIES.'/application/site.php';
 require_once TILDE_PATH_LIBRARIES.'/application/administrator.php';
-require_once TILDE_PATH_LIBRARIES.'/document/document.php';
-require_once TILDE_PATH_LIBRARIES.'/databse/database.php';
-require_once TILDE_PATH_LIBRARIES.'/session/session.php';
-require_once TILDE_PATH_LIBRARIES.'/user/user.php';
-require_once TILDE_PATH_LIBRARIES.'/message/message.php';
 require_once TILDE_PATH_LIBRARIES.'/category/category.php';
-require_once TILDE_PATH_LIBRARIES.'/user/group.php';
-require_once TILDE_PATH_LIBRARIES.'/tag/tag.php';
+require_once TILDE_PATH_LIBRARIES.'/databse/database.php';
+require_once TILDE_PATH_LIBRARIES.'/document/document.php';
 require_once TILDE_PATH_LIBRARIES.'/form/select.php';
+require_once TILDE_PATH_LIBRARIES.'/message/message.php';
+require_once TILDE_PATH_LIBRARIES.'/session/session.php';
+require_once TILDE_PATH_LIBRARIES.'/tag/tag.php';
+require_once TILDE_PATH_LIBRARIES.'/user/group.php';
+require_once TILDE_PATH_LIBRARIES.'/user/user.php';
