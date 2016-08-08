b3eb74091de7ef432b19e006e460b58e69aaa17c
Harris Ingram <harrisdaniel@hotmail.com>
2016-08-08 23:26:52 +1000

    Create factory.php

---
+++ libraries/factory.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @version 0.2.1
+ * @package Tilde.TimeManagement
+ * 
+ * @copyright   Copyright (C) 2016 Team Tilde Time, All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE
+ */
+   
+define('TILDE_TIME', 1);
+
+abstract class TildeFactory{
+	public static $application = null;
+	public static $setting = null;
+	public static $session = null;
+	public static $document = null;
+	public static $database = null;
+	public static $mailer = null;
+	
+	public static function getApplication($id = null){
+		if (!self::$application)
+		{
+			if (!$id)
+			{
+				throw new Exception('Application Instantiation Error', 500);
+			}
+			self::$application = TildeApplicationCms::getInstance($id);
+		}
+		return self::$application;
+	}
+	public static function getSettings(){
+		if (!self::$setting)
+		{
+			self::$setting = new TSettings;
+		}
+		return self::$setting;
+	}
+	public static function getDBObject(){
+		if (!self::$database)
+		{
+			self::$database = self::createDBObject();
+		}
+		return self::$database;
+	}
+	protected static function createDBObject(){
+		
+	}
+}
