d3ea6e1f37b7b1bdb1bb6b793b818fdc83e6675b
Harris Ingram <harrisdaniel@hotmail.com>
2016-08-08 23:13:20 +1000

    Create cms.php

---
+++ libraries/application/cms.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @version 0.2.1
+ * @package Tilde.TimeManagement
+ * 
+ * @copyright   Copyright (C) 2016 Team Tilde Time, All rights reserved.
+ * @license     GNU General Public License version 3 or later; see LICENSE
+ */
+ 
+defined('TILDE_TIME') or die;
+
+class TildeApplicationCms
+{
+	private $className = 'cms';
+    private static $instances = array();
+	private $tables;
+	private $html_title;
+	private $settings;	
+	public static function getInstance($name = null)
+    {
+       if (empty(static::$instances[$name]))
+		{
+			$classname = 'TildeApplication' . ucfirst($name);
+
+			if (!class_exists($classname))
+			{
+				throw new RuntimeException('Requested Application Can Not Be Found', 500);
+			}
+
+			static::$instances[$name] = new $classname;
+		}
+
+		return static::$instances[$name];
+    }
+
+
+    private function __construct()
+    {
+		$this->settings = new TSettings();
+		$this->html_title = $this->settings->sitename;
+    }
+    private function __clone()
+    {
+    }
+
+ 
+    private function __wakeup()
+    {
+    }
+	public function execute(){
+		
+	}
+}
